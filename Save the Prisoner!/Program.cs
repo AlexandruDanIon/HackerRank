cb9c43ec54b2664adfdf632f941b8ca7014c93ec
Alexandru Dan Ion <7117795+AlexandruDanIon@users.noreply.github.com>
2019-12-07 20:31:57 +0200

    -

---
+++ Save the Prisoner!/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+internal class Solution
+{
+    private static void Main(string[] args)
+    {
+        int t = Convert.ToInt32(Console.ReadLine());
+
+        for (int tItr = 0; tItr < t; tItr++)
+        {
+            string[] nms = Console.ReadLine().Split(' ');
+
+            int n = Convert.ToInt32(nms[0]);
+
+            int m = Convert.ToInt32(nms[1]);
+
+            int s = Convert.ToInt32(nms[2]);
+
+            int result = saveThePrisoner(n, m, s);
+
+            Console.WriteLine(result);
+        }
+        Console.ReadKey();
+    }
+
+    // Complete the saveThePrisoner function below.
+    private static int saveThePrisoner(int n, int m, int s)
+    {
+        return ((m - 1) + (s - 1)) % n + 1;
+    }
+}
