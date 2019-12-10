62a29c3efac0550048fd155d46a58e5f5cfb9591
Alexandru Dan Ion <7117795+AlexandruDanIon@users.noreply.github.com>
2019-12-10 14:05:07 +0200

    -

---
+++ Beautiful Days at the Movies/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Diagnostics;
+
+internal class Solution
+{
+    // Complete the beautifulDays function below.
+    private static int BeautifulDays(int i, int j, int k)
+    {
+        int res = 0;
+
+        for (int a = i; a <= j; a++)
+        {
+            int n = a;
+            int r = 0;
+            while (n > 0)
+            {
+                int remainder = n % 10;
+                r = (r * 10) + remainder;
+                n /= 10;
+            }
+
+            if ((a - r) % k == 0)
+                res++;
+        }
+
+        return res;
+    }
+
+    private static void Main(string[] args)
+    {
+        string[] ijk = Console.ReadLine().Split(' ');
+
+        int i = Convert.ToInt32(ijk[0]);
+
+        int j = Convert.ToInt32(ijk[1]);
+
+        int k = Convert.ToInt32(ijk[2]);
+
+        var sw = new Stopwatch();
+        sw.Start();
+        int result = BeautifulDays(i, j, k);
+        sw.Stop();
+        Console.WriteLine(result + Environment.NewLine + sw.ElapsedMilliseconds);
+        Console.ReadKey();
+    }
+}
---
+++ Find digits/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+internal class Solution
+{
+    // Complete the findDigits function below.
+    private static int FindDigits(int n)
+    {
+        return 0;
+    }
+
+    private static void Main(string[] args)
+    {
+        int t = Convert.ToInt32(Console.ReadLine());
+
+        for (int tItr = 0; tItr < t; tItr++)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int result = FindDigits(n);
+
+            Console.WriteLine(result);
+        }
+    }
+}
