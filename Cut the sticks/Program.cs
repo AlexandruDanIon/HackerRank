f245e9363a77870a86270dc9d7537e9d480f7f3a
Alexandru Dan Ion <7117795+AlexandruDanIon@users.noreply.github.com>
2019-12-10 22:59:19 +0200

    -

---
+++ Cut the sticks/Program.cs
@@ -0,0 +1,54 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution
+{
+
+    // Complete the cutTheSticks function below.
+    static int[] cutTheSticks(int[] arr)
+    {
+        var l = arr.ToList();
+        var r = new List<int>();
+        var m = l.Min();
+        
+
+        while(l.Count > 0)
+        {
+            r.Add(l.Count);
+            l.RemoveAll(o => o == m);
+           
+            for (int i = 0; i < l.Count; i++)
+            {
+                l[i] -= m;
+            }
+            m = l.Count() > 0 ? l.Min() : 0 ;
+        }
+
+        return r.ToArray();
+    }
+
+    static void Main(string[] args)
+    {
+
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        int[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp))        ;
+        int[] result = cutTheSticks(arr);
+
+        Console.WriteLine(string.Join("\n", result));
+
+        Console.ReadKey();
+
+    }
+}
---
+++ Library Fine/Program.cs
@@ -0,0 +1,74 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+using System.Diagnostics;
+
+class Solution
+{
+
+    // Complete the libraryFine function below.
+    static int libraryFine(int d1, int m1, int y1, int d2, int m2, int y2)
+    {
+        if (new DateTime(y2, m2, d2) > new DateTime(y1, m1, d1))
+            return 0;
+
+        if (y2 < y1)
+            return 10000;
+        else
+        {
+            if (m2 < m1)
+                return (m1 - m2) * 500;
+            {
+                if (d2 < d1)
+                    return (d1 - d2) * 15;
+            }
+        }
+
+        return 0;
+    }
+
+    static void Main(string[] args)
+    {
+
+        string[] d1M1Y1 = Console.ReadLine().Split(' ');
+
+        int d1 = Convert.ToInt32(d1M1Y1[0]);
+
+        int m1 = Convert.ToInt32(d1M1Y1[1]);
+
+        int y1 = Convert.ToInt32(d1M1Y1[2]);
+
+        string[] d2M2Y2 = Console.ReadLine().Split(' ');
+
+        int d2 = Convert.ToInt32(d2M2Y2[0]);
+
+        int m2 = Convert.ToInt32(d2M2Y2[1]);
+
+        int y2 = Convert.ToInt32(d2M2Y2[2]);
+
+        var sw = new Stopwatch();
+        int result = 0;
+        sw.Start();
+        for (int i = 0; i < 10000000; i++)
+        {
+
+            result = libraryFine(d1, m1, y1, d2, m2, y2);
+        }
+
+        sw.Stop();
+
+        Console.WriteLine(result + Environment.NewLine + sw.ElapsedMilliseconds);
+
+        Console.ReadKey();
+    }
+}
