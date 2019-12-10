bb9b0f92466feeeb74a2c346a12d151e8bca46c7
Alexandru Dan Ion <7117795+AlexandruDanIon@users.noreply.github.com>
2019-12-10 19:19:20 +0200

    -

---
+++ Sequence Equation/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Linq;
+using System;
+
+class Solution
+{
+
+    static int[] PermutationEquation(int[] p)
+    {
+        var y = new int[p.Length];
+
+        for (int i = 1; i <= p.Length; i++)
+        {
+            y[i - 1] = Array.IndexOf(p, Array.IndexOf(p, i) + 1) + 1 ;
+        }
+
+        return y;
+    }
+
+    static void Main(string[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        int[] p = Array.ConvertAll(Console.ReadLine().Split(' '), pTemp => Convert.ToInt32(pTemp))
+        ;
+        int[] result = PermutationEquation(p);
+
+        Console.WriteLine(string.Join("\n", result));
+
+        Console.ReadKey();
+
+    }
+}
